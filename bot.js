fa774548309347a5d04eaac07626aec8c7847e26
BeXiL-N1 <68783480+BeXiL-N1@users.noreply.github.com>
2020-07-25 17:39:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['Your ID']
+const myid = ['268092347642806272']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
